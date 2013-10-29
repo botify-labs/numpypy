d751c788f25daf536daac9b6cfc0762072e3f4cc
Brian Kearns <bdkearns@gmail.com>
2013-10-29 04:44:01 -0400

    add a dummy format_longfloat

--- numpy/core/multiarray.py
+++ numpy/core/multiarray.py
@@ -4,6 +4,9 @@ def _fastCopyAndTranspose(a):
 def copyto(dst, src, casting='same_kind', where=None):
     dst.fill(src)
 
+def format_longfloat(x, precision):
+    return "%%.%df" % precision % x
+
 def set_typeDict(d):
     pass
 
@@ -14,7 +17,7 @@ CLIP WRAP RAISE MAXDIMS ALLOW_THREADS BUFSIZE nditer nested_iters
 broadcast empty_like fromiter fromfile frombuffer newbuffer getbuffer
 int_asbuffer set_numeric_ops can_cast promote_types
 min_scalar_type result_type lexsort compare_chararrays putmask einsum inner
-_vec_string datetime_data format_longfloat
+_vec_string datetime_data
 datetime_as_string busday_offset busday_count is_busday busdaycalendar
 _flagdict flagsobj may_share_memory
 '''.split():
