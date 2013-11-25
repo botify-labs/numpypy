6b8df83c058c66a1a7c40f241dd270de5aa15361
Matti Picus <matti.picus@gmail.com>
2013-11-26 00:11:52 +0200

    enable linalg._umath_linalg extension module

--- numpy/linalg/setup.py
+++ numpy/linalg/setup.py
@@ -42,13 +42,12 @@ def configuration(parent_package='',top_path=None):
 
     # umath_linalg module
 
-    if '__pypy__' not in sys.builtin_module_names:
-        config.add_extension('_umath_linalg',
-                         sources = [get_lapack_lite_sources],
-                         depends =  ['umath_linalg.c.src'] + lapack_lite_src,
-                         extra_info = lapack_info,
-                         libraries = ['npymath'],
-                         )
+    config.add_extension('_umath_linalg',
+                     sources = [get_lapack_lite_sources],
+                     depends =  ['umath_linalg.c.src'] + lapack_lite_src,
+                     extra_info = lapack_info,
+                     libraries = ['npymath'],
+                     )
 
     return config
 
