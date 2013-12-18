209bfe00836c51ede08b8d4338e64e1d899603de
Brian Kearns <bdkearns@gmail.com>
2013-12-18 02:56:35 -0500

    we have no datetime64

--- numpy/lib/_iotools.py
+++ numpy/lib/_iotools.py
@@ -533,8 +533,8 @@ class StringConverter(object):
     @classmethod
     def _dtypeortype(cls, dtype):
         """Returns dtype for datetime64 and type of dtype otherwise."""
-        if dtype.type == np.datetime64:
-            return dtype
+        #if dtype.type == np.datetime64:
+        #    return dtype
         return dtype.type
     #
     @classmethod
