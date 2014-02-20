fb6a75dc4ad1f47041c666ecca2104df85ef2701
Brian Kearns <bdkearns@gmail.com>
2014-02-20 01:46:58 -0500

    iscomplexobj can throw NotImplErr when callled with a dtype instance

--- numpy/testing/utils.py
+++ numpy/testing/utils.py
@@ -280,7 +280,7 @@ def assert_equal(actual,desired,err_msg='',verbose=True):
     # XXX: catch ValueError for subclasses of ndarray where iscomplex fail
     try:
         usecomplex = iscomplexobj(actual) or iscomplexobj(desired)
-    except ValueError:
+    except (ValueError, NotImplementedError):
         usecomplex = False
 
     if usecomplex:
