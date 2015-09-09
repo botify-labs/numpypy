744992623eb9f235d3b82b170b8155b17d00752e
Ronan Lamy <ronan.lamy@gmail.com>
2015-09-09 20:06:34 +0100

    Extract creation of the cffi wrapper to _umath_linalg_build.py

--- numpy/linalg/_umath_linalg.py
+++ numpy/linalg/_umath_linalg.py
@@ -3,38 +3,8 @@
 # of the pypy extended frompyfunc, so the _umath_linag_capi functions are always called
 # with the final arguments, no broadcasting needed.
 
-import sys, os
-import cffi
+from ._umath_linalg_build import ffi, umath_linalg_capi, all_four, three
 
-ffi = cffi.FFI()
-ffi.cdef('''
-    void init_constants(void);
-    int _npy_clear_floatstatus(void);
-    int _npy_set_floatstatus_invalid(void);
-''')
-
-ufunc_cdef = 'void %s(char **args, intptr_t * dimensions, intptr_t * steps, void*);'
-all_four = ['FLOAT_', 'DOUBLE_', 'CFLOAT_', 'CDOUBLE_']
-three = ['FLOAT_', 'DOUBLE_', 'CDOUBLE_']
-base_four_names = ['slogdet','det','inv', 'solve1', 'solve', 'eighup',
-                    'eighlo', 'eigvalshlo', 'eigvalshup', 'cholesky_lo',
-                    'svd_A', 'svd_S', 'svd_N'
-                    ]
-base_three_names = ['eig', 'eigvals']
-names = []
-for name in base_four_names:
-    names += [pre + name for pre in all_four]
-for name in base_three_names:
-    names += [pre + name for pre in three]
-for name in names:
-    ffi.cdef(ufunc_cdef % name)
-
-# TODO macos?
-if sys.platform == 'win32':
-    so_name = '/umath_linalg_cffi.dll'
-else:
-    so_name = '/libumath_linalg_cffi.so'
-umath_linalg_capi = ffi.dlopen(os.path.dirname(__file__) + so_name)
 umath_linalg_capi.init_constants()
 
 import numpy as np
