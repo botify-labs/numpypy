83197070e0d94555d7b2ecb263045db972647078
Ronan Lamy <ronan.lamy@gmail.com>
2015-10-04 17:16:29 +0100

    Just #include the header in _fft_build.py instead of copying it

--- numpy/fft/_fft_build.py
+++ numpy/fft/_fft_build.py
@@ -12,34 +12,4 @@ void rfftb(int n, double r[], double wsave[]);
 ''')
 
 
-ffi.set_source('numpy.fft._fft_cffi', '''
-/* fftpack.h */
-/*
- * This file is part of tela the Tensor Language.
- * Copyright (c) 1994-1995 Pekka Janhunen
- */
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define DOUBLE
-
-#ifdef DOUBLE
-#define Treal double
-#else
-#define Treal float
-#endif
-
-extern void cfftf(int N, Treal data[], const Treal wrk[]);
-extern void cfftb(int N, Treal data[], const Treal wrk[]);
-extern void cffti(int N, Treal wrk[]);
-
-extern void rfftf(int N, Treal data[], const Treal wrk[]);
-extern void rfftb(int N, Treal data[], const Treal wrk[]);
-extern void rffti(int N, Treal wrk[]);
-
-#ifdef __cplusplus
-}
-#endif
-''')
+ffi.set_source('numpy.fft._fft_cffi', '#include "fftpack.h"')
