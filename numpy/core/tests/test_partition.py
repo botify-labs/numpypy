3903aba0c844c32bcdc920846792cfaa92736f1c
Sergey Matuynin <sbmatyunin@gmail.com>
2016-03-12 20:29:03 +0100

    Test TestPartition.test_uint16 is compatible with CPython

--- numpy/core/tests/test_partition.py
+++ numpy/core/tests/test_partition.py
@@ -2,6 +2,11 @@ from __future__ import division, absolute_import, print_function
 
 import numpy as np
 from numpy.testing import TestCase
+import sys
+
+
+def is_pypy():
+    return '__pypy__' in sys.builtin_module_names
 
 
 class TestPartition(TestCase):
@@ -11,8 +16,12 @@ class TestPartition(TestCase):
     def test_uint16(self):
         arr = np.arange(10, -1, -1, dtype='int16')
 
-        with self.assertRaises(NotImplementedError):
-            np.partition(arr, self.pivot_index)
+        if is_pypy():
+            with self.assertRaises(NotImplementedError):
+                np.partition(arr, self.pivot_index)
+        else:
+            partition = np.partition(arr, self.pivot_index)
+            self._check_partition(partition, self.pivot_index, self.pivot)
 
     def test_uint32(self):
         arr = np.arange(10, -1, -1, dtype='int32')
