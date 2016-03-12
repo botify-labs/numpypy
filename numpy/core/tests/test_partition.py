5310144d50b7800d91795290c9e4b1d4574e6aeb
Sergey Matuynin <sbmatyunin@gmail.com>
2016-03-12 01:56:41 +0100

    Added test for numpy.partition of unsigned int

---
+++ numpy/core/tests/test_partition.py
@@ -0,0 +1,34 @@
+from __future__ import division, absolute_import, print_function
+
+import numpy as np
+from numpy.testing import TestCase
+
+
+class TestPartition(TestCase):
+    pivot = 2
+    pivot_index = 2
+
+    def test_uint16(self):
+        arr = np.arange(10, -1, -1, dtype='int16')
+
+        with self.assertRaises(NotImplementedError):
+            np.partition(arr, self.pivot_index)
+
+    def test_uint32(self):
+        arr = np.arange(10, -1, -1, dtype='int32')
+
+        partition = np.partition(arr, self.pivot_index)
+
+        self._check_partition(partition, self.pivot_index, self.pivot)
+
+    def test_uint64(self):
+        arr = np.arange(10, -1, -1, dtype='int64')
+
+        partition = np.partition(arr, self.pivot_index)
+
+        self._check_partition(partition, self.pivot_index, self.pivot)
+
+    def _check_partition(self, partition, pivot_index, pivot):
+        self.assertTrue(np.all(partition[:pivot_index] < partition[pivot_index]))
+        self.assertTrue(np.all(partition[pivot_index:] >= partition[pivot_index]))
+        self.assertTrue(partition[pivot_index] == pivot)
