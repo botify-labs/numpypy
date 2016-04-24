df821bc0de53ba167e3bedda4cc5550f59558955
mattip <matti.picus@gmail.com>
2016-04-24 09:17:45 +0300

    revert part of 51920cf; support PyPy < 5.1.0

--- numpy/lib/utils.py
+++ numpy/lib/utils.py
@@ -37,6 +37,15 @@ def get_include():
         ...
 
     """
+    if '__pypy__' in sys.builtin_module_names and not (
+            sys.pypy_version_info.major >= 5 and
+            sys.pypy_version_info.minor >= 1):
+        import distutils.sysconfig
+        # numpy/*.h is already shipped with PyPy includes, no need for
+        # numpy/core/include/*. Moreover, those files are specific for the
+        # CPython version of numpy, which means that if we include them on
+        # PyPy by mistake, we get most probably a segfault.
+        return distutils.sysconfig.get_python_inc()
     import numpy
     if numpy.show_config is None:
         # running from numpy source directory
